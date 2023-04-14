52f24c61119e46d5cb0fce7a30488e33756fff38
Marcio Faria <m.faria.jr@icloud.com>
2023-04-13 21:16:12 -0300

    first commit

---
+++ src/test/java/br/edu/univas/tasi/prova/MarcioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univas.tasi.prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

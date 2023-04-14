4e0665dbdf12f1252631e6e9db2481cb38ba327d
Marcio Faria <m.faria.jr@icloud.com>
2023-04-13 22:00:08 -0300

    enviando classes

--- src/main/java/br/edu/univas/tasi/prova/controller/MaterialController.java
+++ src/main/java/br/edu/univas/tasi/prova/controller/MaterialController.java
@@ -1,5 +1,27 @@
 package br.edu.univas.tasi.prova.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.univas.tasi.prova.service.MaterialService;
+
+@RestController
+@RequestMapping("/materials")
 public class MaterialController {
 
+	
+	@Autowired
+	private MaterialService service;
+	
+	
+	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createMaterial() {
+		
+		
+	}
 }
--- src/main/java/br/edu/univas/tasi/prova/service/MaterialService.java
+++ src/main/java/br/edu/univas/tasi/prova/service/MaterialService.java
@@ -1,6 +1,22 @@
 package br.edu.univas.tasi.prova.service;
 
-public class MaterialService {
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import br.edu.univas.tasi.prova.entities.MaterialEntity;
+import br.edu.univas.tasi.prova.repository.MaterialRepository;
+
+public class MaterialService {
 	
+	private MaterialRepository repo;
+	
+	@Autowired
+	public MaterialService(MaterialRepository repo) {
+		this.repo = repo;
+	}
+
+	public void createProduct(MaterialEntity material) {
+		repo.save(material);
+	}
 }
